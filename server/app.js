70baf0e0cb9b07449bdfcf3eae1d2c00d1741da2
Sai kumar <saikumartelang01@gmail.com>
2026-01-30 18:40:26 +0530

    server fix

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://vet-care-delta.vercel.app/"
+    "https://vet-care-delta.vercel.app/api"
   ],
   credentials: true
 }));
