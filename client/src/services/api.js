f77723308658faf855788d1e73c7766b39c4d5c3
Sai kumar <saikumartelang01@gmail.com>
2026-01-31 19:47:30 +0530

    render

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://vetcare-qzor.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/"
 });
 
 export const createAppointment = (data) =>
