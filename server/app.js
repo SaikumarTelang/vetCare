c02a8bdf5380164ae6d7b3d32acdb2c6f33a3d0d
Sai kumar <saikumartelang01@gmail.com>
2026-01-31 19:21:44 +0530

    render

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://vet-care-inky.vercel.app/"
+    "https://vetcare-qzor.onrender.com"
   ],
   credentials: true
 }));
