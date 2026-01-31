595275356e75bf9add4f3ca02bda32bd323aec92
Sai kumar <saikumartelang01@gmail.com>
2026-01-31 19:52:33 +0530

    render

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 /* ========= MIDDLEWARE ========= */
 app.use(cors({
   origin: [
-    // "http://localhost:5000",
+    "http://localhost:3000",
     "https://vet-care-seven-git-main-sai-kumars-projects-175175a8.vercel.app/",
     "https://vet-care-seven-luc66br5p-sai-kumars-projects-175175a8.vercel.app/",
     "https://vet-care-seven.vercel.app/"
