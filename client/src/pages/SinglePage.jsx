0ab9783afe70f082e988f82f0165750ea0529f4d
Sai kumar <saikumartelang01@gmail.com>
2026-02-04 21:10:46 +0530

    changed UI design

--- client/src/pages/SinglePage.jsx
+++ client/src/pages/SinglePage.jsx
@@ -10,6 +10,14 @@ export default function SinglePage() {
     const el = document.getElementById(id);
     if (el) el.scrollIntoView({ behavior: "smooth", block: "start" });
   };
+  useEffect(() => {
+  const hash = location.hash?.replace("#", "");
+  if (hash) scrollTo(hash);
+  else if (location.pathname === "/") {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+}, [location.hash, location.pathname]);
+
 
   const location = useLocation();
   useEffect(() => {
